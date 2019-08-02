af029978cce382da801be0614f66501b6ac49063
Ana Carolina Comandulli <ana.comandulli@wdc.com>
2019-08-01 17:15:01 -0700

    joining tiq and gtm

--- tiq/wd-tiq-body.js
+++ tiq/wd-tiq-body.js
@@ -1,7 +1,9 @@
 
 
-loadTiQBody('//tags.tiqcdn.com/utag/wd/westerndigital/qa/utag.js');
+window.utag_data = window.utag_data || {};
 
+// call Tealium TiQ analytics
+loadTiQBody('//tags.tiqcdn.com/utag/wd/westerndigital/qa/utag.js');
 function loadTiQBody (url){
   var script = document.createElement('script');
   script.src = url;
@@ -11,3 +13,10 @@ function loadTiQBody (url){
   location.insertBefore(script, list.childNodes[0]);
 
 }
+
+// call GTM analytics
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MB43D4S');
